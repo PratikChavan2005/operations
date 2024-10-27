9b631a65d94f5fcbc1d7b0d97f8962ef77870b53
PratikChavan2005 <pratikchavan963744@gmail.com>
2024-10-27 22:28:28 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,39 @@
+  public class operations
+{
+   public static void main(String[] args)
+   {
+     	int num1=10,num2=20,res;
+
+     	res=num1+num2;
+	System.out.println("Addition is =" +res);
+	res=num1-num2;
+	System.out.println("Subtraction is ="+res);
+	res=num1*num2;
+	System.out.println("Multiplication is ="+res);
+	res=num1/num2;
+	System.out.println("Division is ="+res);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
